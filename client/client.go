b9b0a4f0992522cc4ba2368fcf1ee61909234830
rjkris <ss-zcli@pku.edu.cn>
2022-06-04 17:18:07 +0800

    请求重定向为https

--- client/client.go
+++ client/client.go
@@ -4,13 +4,14 @@ import (
 	"github.com/cloudflare/cfssl/log"
 	"github.com/gin-gonic/gin"
 	"github.com/ssbcV2/global"
+	"github.com/unrolled/secure"
 )
 
 // 监听用户请求
 func ListenRequest() {
 	r := gin.Default()
 	r.Use(Cors()) // 使用跨域组件
-
+	//r.Use(TlsHandler()) // 重定向为https
 	r.POST("/postTran", postTran)              // 提交一笔交易
 	r.GET("/registerAccount", registerAccount) // 注册账户
 	r.POST("/postCrossTran", postCrossTran)    // 提交一笔跨链交易
@@ -26,4 +27,25 @@ func ListenRequest() {
 	log.Info(" ---------------------------------------------------------------------------------")
 	log.Info("|  已启动PBFT客户端，请启动全部节点后再发送消息！  |")
 	log.Info(" ---------------------------------------------------------------------------------")
+}
+
+func TlsHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		secureMiddleware := secure.New(secure.Options{
+			SSLRedirect: true,
+			SSLHost:     "localhost:8080",
+		})
+		err := secureMiddleware.Process(c.Writer, c.Request)
+
+		// If there was an error, do not continue.
+		if err != nil {
+			c.Abort()
+			return
+		}
+		// Avoid header rewrite if response is a redirection.
+		//if status := c.Writer.Status(); status > 300 && status < 399 {
+		//	c.Abort()
+		//}
+		c.Next()
+	}
 }
--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/spf13/afero v1.8.1 // indirect
 	github.com/spf13/viper v1.10.1
 	github.com/syndtr/goleveldb v1.0.0
+	github.com/unrolled/secure v1.10.0
 	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/ini.v1 v1.66.4 // indirect
